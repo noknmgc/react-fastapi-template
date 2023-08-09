71f2e40f0a81cb5735c558da03a413cc52684139
Naoki Nomaguchi <noknmgc@gmail.com>
2023-08-09 14:05:10 +0900

    samll fix:remove unused import

--- frontend/my-app/src/features/Login/index.tsx
+++ frontend/my-app/src/features/Login/index.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-import TextInput from "../../common/components/input/TextInput";
 import LabeledInput from "../../common/components/input/LabeledInput";
 
 import { User } from "../../common/types";
