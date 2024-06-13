f771d387769f2103c72ff25f7f817dd1e2d399ea
Naoki Nomaguchi <noknmgc@gmail.com>
2024-06-13 16:33:00 +0900

    ✅ debug for api client

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { QueryClientProvider } from "@tanstack/react-query";
+
+import { queryClient } from "./common/api/react-query";
+import { useMyself } from "./common/api/useMyself";
+import { authApi } from "./common/api/clients";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  authApi.loginCookie("test", "test");
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <QueryClientProvider client={queryClient}>
+      <Test />
+    </QueryClientProvider>
+  );
 }
 
-export default App
+const Test: React.FC = () => {
+  const { data, isLoading, isError } = useMyself();
+
+  if (isLoading) return <div>Loading...</div>;
+  if (isError) return <div>Something happened!</div>;
+  if (!data) return <div>Data is undefined.</div>;
+  return <div>{data.username}</div>;
+};
+export default App;
