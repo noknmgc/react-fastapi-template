263e3cef228c51d0df53439cda32e744f59b9111
Naoki Nomaguchi <noknmgc@gmail.com>
2024-06-13 16:27:12 +0900

    ✨ add alias & proxy server

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,9 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [{ find: "@", replacement: "/src" }],
+  },
   server: {
     host: true, // 開発サーバーを公開
     port: 3000, // ポート3000
@@ -11,6 +14,14 @@ export default defineConfig({
       // 開発サーバーのリフレッシュ間隔を大きくする(負荷軽減のため)
       usePolling: true,
       interval: 1000,
+    },
+    proxy: {
+      // 開発サーバーをAPIのプロキシサーバーとする
+      "/api": {
+        target: "http://backend:8000",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
     },
   },
 });
