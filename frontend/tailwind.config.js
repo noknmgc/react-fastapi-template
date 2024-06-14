bf0405a84422ae393f5901e81a2827e6225dff3d
Naoki Nomaguchi <noknmgc@gmail.com>
2024-06-14 12:08:45 +0900

    ✨ Implement merge classname

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      theme: {
+        colors: {
+          primary: {
+            light: "#5d92e3",
+            DEFAULT: "#102C57",
+            dark: "#051938",
+          },
+          accent: {
+            light: "#ffe0e0",
+            DEFAULT: "#FFB1B1",
+            dark: "#cc7e7e",
+          },
+        },
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography")],
 };
