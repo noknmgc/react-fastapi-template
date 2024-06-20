c9b83439d9cf5ea3418359318b9f49b903d9a1ae
Naoki Nomaguchi <noknmgc@gmail.com>
2024-06-20 18:03:35 +0900

    ♻️ Refactor get todo

--- frontend/src/features/todo/api/useTodo.ts
+++ frontend/src/features/todo/api/useTodo.ts
@@ -1,12 +1,15 @@
 import { queryOptions, useQuery } from "@tanstack/react-query";
 import { todosApi } from "@/common/api/clients";
 
-export const todoQueryOptions = (todoId: number) =>
+const getTodo = (todoId: number) =>
+  todosApi.readMyTodo(todoId).then((r) => r.data);
+
+export const getTodoQueryOptions = (todoId: number) =>
   queryOptions({
     queryKey: ["my-todo", todoId],
-    queryFn: () => todosApi.readMyTodo(todoId).then((r) => r.data),
+    queryFn: () => getTodo(todoId),
   });
 
 export const useTodo = (todoId: number) => {
-  return useQuery({ ...todoQueryOptions(todoId) });
+  return useQuery({ ...getTodoQueryOptions(todoId) });
 };
--- frontend/src/features/todo/api/useTodos.ts
+++ frontend/src/features/todo/api/useTodos.ts
@@ -1,12 +1,14 @@
 import { queryOptions, useQuery } from "@tanstack/react-query";
 import { todosApi } from "@/common/api/clients";
 
-export const todosQueryOptions = () =>
+const getTodos = () => todosApi.readMyTodos().then((r) => r.data);
+
+export const getTodosQueryOptions = () =>
   queryOptions({
     queryKey: ["my-todos"],
-    queryFn: () => todosApi.readMyTodos().then((r) => r.data),
+    queryFn: () => getTodos(),
   });
 
 export const useTodos = () => {
-  return useQuery({ ...todosQueryOptions() });
+  return useQuery({ ...getTodosQueryOptions() });
 };
