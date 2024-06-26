dfc688692fe85edf7089d4bb994e60fb80194cff
Naoki Nomaguchi <noknmgc@gmail.com>
2024-06-26 16:33:14 +0900

    ♻️ Change query key

--- frontend/src/features/todo/api/useTodo.ts
+++ frontend/src/features/todo/api/useTodo.ts
@@ -1,12 +1,13 @@
 import { queryOptions, useQuery } from "@tanstack/react-query";
 import { todosApi } from "@/common/api/clients";
+import { getTodosQueryOptions } from "./useTodos";
 
 const getTodo = (todoId: number) =>
   todosApi.readMyTodo(todoId).then((r) => r.data);
 
 export const getTodoQueryOptions = (todoId: number) =>
   queryOptions({
-    queryKey: ["my-todo", todoId],
+    queryKey: [getTodosQueryOptions().queryKey, todoId],
     queryFn: () => getTodo(todoId),
   });
 
