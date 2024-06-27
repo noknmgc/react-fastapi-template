dd2643a57bcb3b01aac70699efcdc3bbfe86b563
Naoki Nomaguchi <noknmgc@gmail.com>
2024-06-27 14:52:27 +0900

    ✨ Implement global dialog

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import { QueryClientProvider } from "@tanstack/react-query";
 
 import { queryClient } from "./common/api/react-query";
+import { Dialog } from "./common/components/dialog";
 import { AppRouter } from "./routes";
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <AppRouter />
+      <Dialog />
     </QueryClientProvider>
   );
 }
