9a1e86a1416a1461b4bdadcc29c3b5738a77fe0b
Naoki Nomaguchi <noknmgc@gmail.com>
2024-06-27 17:52:15 +0900

    💄

--- frontend/src/routes/errors/Forbidden.tsx
+++ frontend/src/routes/errors/Forbidden.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const Forbidden: React.FC = () => {
   return (
-    <div className="prose lg:prose-xl">
+    <div className="prose mx-auto mt-8 max-w-screen-md lg:prose-xl lg:max-w-screen-lg">
       <h1>403 - Forbidden</h1>
       <p>アクセスしようとしたページは、表示できません。</p>
       <Link to="/" replace>
--- frontend/src/routes/errors/NotFound.tsx
+++ frontend/src/routes/errors/NotFound.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const NotFound: React.FC = () => {
   return (
-    <div className="prose lg:prose-xl mx-auto max-w-screen-md lg:max-w-screen-lg">
+    <div className="prose mx-auto mt-8 max-w-screen-md lg:prose-xl lg:max-w-screen-lg">
       <h1>404 - Not Found</h1>
       <p>お探しのページは見つかりませんでした。</p>
       <Link to="/" replace>
